e25cf64ec334300355384c3cd8952b1a641e945e
dreamkid <dreamkid0924@163.com>
2017-09-18 17:02:19 +0800

    co

--- app/src/main/java/com/savor/zhixiang/activity/SplashActivity.java
+++ app/src/main/java/com/savor/zhixiang/activity/SplashActivity.java
@@ -1,15 +1,39 @@
 package com.savor.zhixiang.activity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 import com.savor.zhixiang.R;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+//        setContentView(R.layout.activity_splash);
+
+        // 因为启动页设置主题背景图片以后，渐隐的动画被遮罩看不到，所有放到欢迎页做
+        Intent intent = new Intent(SplashActivity.this,WelcomActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    @Override
+    public void getViews() {
+
+    }
+
+    @Override
+    public void setViews() {
+
+    }
+
+    @Override
+    public void setListeners() {
+
     }
 }
---
+++ app/src/main/java/com/savor/zhixiang/activity/WelcomActivity.java
@@ -0,0 +1,61 @@
+package com.savor.zhixiang.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+
+import com.savor.zhixiang.R;
+
+public class WelcomActivity extends BaseActivity {
+
+    private ImageView mSplashIv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcom);
+
+        getViews();
+        setViews();
+        setListeners();
+    }
+
+    @Override
+    public void getViews() {
+        mSplashIv = findViewById(R.id.iv_splash);
+    }
+
+    @Override
+    public void setViews() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f,0f);
+        alphaAnimation.setDuration(1500);
+        alphaAnimation.setFillAfter(true);
+        alphaAnimation.setInterpolator(new DecelerateInterpolator());
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(WelcomActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mSplashIv.startAnimation(alphaAnimation);
+    }
+
+    @Override
+    public void setListeners() {
+
+    }
+}

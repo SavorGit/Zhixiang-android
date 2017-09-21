9c31685feb6644e4f79391a6d51d2788248409eb
bushlee <li.cong@savor.cn>
2017-09-21 15:42:26 +0800

    UI初始化

--- app/src/main/java/com/savor/zhixiang/activity/MyCollectActivity.java
+++ app/src/main/java/com/savor/zhixiang/activity/MyCollectActivity.java
@@ -1,12 +1,19 @@
 package com.savor.zhixiang.activity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.common.api.utils.ShowMessage;
+import com.common.api.widget.pulltorefresh.library.PullToRefreshBase;
+import com.savor.zhixiang.core.ApiRequestListener;
+import com.savor.zhixiang.core.AppApi;
+import com.savor.zhixiang.core.ResponseErrorMessage;
 
 /**
  * Created by bushlee on 2017/9/21.
  */
 
-public class MyCollectActivity extends BaseActivity{
+public class MyCollectActivity extends BaseActivity implements View.OnClickListener,ApiRequestListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,4 +33,38 @@ public class MyCollectActivity extends BaseActivity{
     public void setListeners() {
 
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    PullToRefreshBase.OnRefreshListener onRefreshListener = new PullToRefreshBase.OnRefreshListener() {
+        @Override
+        public void onRefresh(PullToRefreshBase refreshView) {
+
+        }
+    };
+
+    PullToRefreshBase.OnLastItemVisibleListener onLastItemVisibleListener = new PullToRefreshBase.OnLastItemVisibleListener() {
+        @Override
+        public void onLastItemVisible() {
+
+        }
+    };
+    @Override
+    public void onSuccess(AppApi.Action method, Object obj) {
+
+    }
+
+    @Override
+    public void onError(AppApi.Action method, Object obj) {
+
+        if(obj instanceof ResponseErrorMessage) {
+            ResponseErrorMessage message = (ResponseErrorMessage) obj;
+            String msg = message.getMessage();
+            ShowMessage.showToast(this,msg);
+        }
+    }
+
 }

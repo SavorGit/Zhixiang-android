c53ac135ae48b2afb5e55d7fd2f0dfc47f06dd98
bushlee <li.cong@savor.cn>
2017-09-21 15:21:21 +0800

    commit

---
+++ app/src/main/java/com/savor/zhixiang/activity/MyCollectActivity.java
@@ -0,0 +1,29 @@
+package com.savor.zhixiang.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by bushlee on 2017/9/21.
+ */
+
+public class MyCollectActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void getViews() {
+
+    }
+
+    @Override
+    public void setViews() {
+
+    }
+
+    @Override
+    public void setListeners() {
+
+    }
+}

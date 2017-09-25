ce410fd75aca78bcd99ee3ba91c277f45deed9c7
bushlee <li.cong@savor.cn>
2017-09-25 15:03:05 +0800

    UI初始化

--- app/src/main/java/com/savor/zhixiang/activity/MyCollectActivity.java
+++ app/src/main/java/com/savor/zhixiang/activity/MyCollectActivity.java
@@ -1,34 +1,64 @@
 package com.savor.zhixiang.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.common.api.utils.ShowMessage;
 import com.common.api.widget.pulltorefresh.library.PullToRefreshBase;
+import com.common.api.widget.pulltorefresh.library.PullToRefreshListView;
 import com.savor.zhixiang.R;
+import com.savor.zhixiang.adapter.AllListAdapter;
+import com.savor.zhixiang.bean.ListItem;
 import com.savor.zhixiang.core.ApiRequestListener;
 import com.savor.zhixiang.core.AppApi;
 import com.savor.zhixiang.core.ResponseErrorMessage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by bushlee on 2017/9/21.
  */
 
 public class MyCollectActivity extends BaseActivity implements View.OnClickListener,ApiRequestListener {
+    private Context context;
+    private String collecTime = "";
+    private PullToRefreshListView mPullRefreshListView;
+    private AllListAdapter mAdapter;
+    private TextView mRefreshDataHinttv;
+    private TextView tv_center;
+    private boolean isUp = true;
+    private List<ListItem> list = new ArrayList<ListItem>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collect);
+        context = this;
+        getViews();
+        setViews();
+        setListeners();
+        getData();
     }
 
     @Override
     public void getViews() {
 
+        mPullRefreshListView = (PullToRefreshListView)findViewById(R.id.listview);
+        mRefreshDataHinttv = (TextView) findViewById(R.id.tv_refresh_data_hint);
+        tv_center = (TextView) findViewById(R.id.tv_center);
     }
 
     @Override
     public void setViews() {
-
+        mAdapter = new AllListAdapter(context);
+        // mListView = mPullRefreshListView.getRefreshableView();
+        mPullRefreshListView.setAdapter(mAdapter);
     }
 
     @Override
@@ -41,6 +71,10 @@ public class MyCollectActivity extends BaseActivity implements View.OnClickListe
 
     }
 
+    private void getData(){
+        AppApi.getMyCollection(this,collecTime,this);
+
+    }
     PullToRefreshBase.OnRefreshListener onRefreshListener = new PullToRefreshBase.OnRefreshListener() {
         @Override
         public void onRefresh(PullToRefreshBase refreshView) {
@@ -67,6 +101,48 @@ public class MyCollectActivity extends BaseActivity implements View.OnClickListe
             String msg = message.getMessage();
             ShowMessage.showToast(this,msg);
         }
+    }
+
+    private void handleVodList(List<ListItem> mList){
+
+        if (mList != null && mList.size() > 0) {
+            mPullRefreshListView.setVisibility(View.VISIBLE);
+            if (isUp) {
+                list.clear();
+                mAdapter.clear();
+                mPullRefreshListView.onLoadComplete(true,false);
+
+            }else {
+                mPullRefreshListView.onLoadComplete(true,false);
+            }
+            collecTime = mList.get(mList.size()-1).getCollecTime()+"";
+            list.addAll(mList);
+            mAdapter.setData(list);
+//            int haveNext = 0;
+//            haveNext =  listResult.getNextpage();
+//
+//            if (haveNext==0) {
+//                mPullRefreshListView.onLoadComplete(false,false);
+//            }else {
+//                mPullRefreshListView.onLoadComplete(true,false);
+//            }
+
+            if (mList!=null && mList.size()<20) {
+                mPullRefreshListView.onLoadComplete(false,false);
+            }else {
+                mPullRefreshListView.onLoadComplete(true,false);
+            }
+        }else {
+//            if (list != null && list.size()>0  ) {
+//                mAdapter.clear();
+//            }
+            //mPullRefreshListView.setVisibility(View.GONE);
+
+            mPullRefreshListView.onLoadComplete(false,true);
+        }
+
+
+
     }
 
 }
